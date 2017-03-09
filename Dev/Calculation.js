0b322c4eeae9b72a3d6edf82d1e945221464bbc8
Gmcccccc <gmc2777522@outlook.com>
2017-03-09 15:14:30 -0500

    Code update
    index.html:
    Add score to display current score.
    
    style.css:
    styleing score class
    
    Calculation.js:
    description of every function
    
    Deleted Init.js  and maybe replaced with reset.js later.

--- Dev/Calculation.js
+++ Dev/Calculation.js
@@ -3,3 +3,23 @@
  */
 
 // This file is used to calculate the score of game
+
+
+
+/*function is used to call MoveLine function to move block for all rows or columns.*/
+var calculate = function(direction)
+{
+
+}
+
+/* A function is used to calculate the score on current table. Score for every block follows the above standard.*/
+var CalculateScore = function()
+{
+
+}
+
+/*A function is used to display the num on every block.*/
+var show = function()
+{
+
+}
--- Dev/Init.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Leo.H & MC.Gong on 2017/3/6.
- */
-
-
-// This file is used to initialize the game.
