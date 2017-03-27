ddcb63df01c4fb98c10286b760dda7f0025c46e8
Gmcccccc <gmc2777522@outlook.com>
2017-03-27 15:03:15 -0400

    Deleted jquery-2.1.1.min.js and CheckMovement files

--- Dev/Init.js
+++ Dev/Init.js
@@ -15,55 +15,6 @@ var BlockTable = [
 $(document).ready(function ()
 {
     refresh();
-    /*$(document).keydown(function (event)
-    {
-        //if(!noMove())
-		//{
-			//Move Up
-			if(event.keyCode == 38)
-			{
-				if(canMoveUp()){
-					UpMoving();
-					show();
-					showValue(getRandomFreeCell(),getRandomNumber());
-				}
-
-			}
-
-			//Move Down
-			if(event.keyCode == 40)
-			{
-				if(canMoveDown(())
-				{
-					DownMoving();
-					show();
-					showValue(getRandomFreeCell(),getRandomNumber());
-				}
-			}
-
-			//Move Left
-			if(event.keyCode == 37)
-			{
-				if(canMoveLeft(())
-				{
-					LeftMoving();
-					show();
-					showValue(getRandomFreeCell(),getRandomNumber());
-				}
-			}
-
-			//Move Right
-			if(event.keyCode == 49)
-			{
-				if(canMoveRight())
-				{
-					RightMoving();
-					show();
-					showValue(getRandomFreeCell(),getRandomNumber());
-				}
-			}
-		//}
-	});*/
 });
 
 //play again
